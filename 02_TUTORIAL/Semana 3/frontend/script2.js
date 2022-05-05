c6b02a12a4f77cfda98b58c90f8ee718325582e2
MateusGCN <mateus.neves@sou.inteli.edu.br>
2022-05-05 13:08:56 -0300

    curriculo semana3 com css e bootstrap

---
+++ 02_TUTORIAL/Semana 3/frontend/script2.js
@@ -0,0 +1,6 @@
+﻿function numero(){
+    let numero1 = parseFloat(document.getElementById("1").value)
+    let numero2 = parseFloat(document.getElementById("2").value)
+    let resultado = document.getElementById("resultado")
+    resultado.innerHTML=`${numero1} + ${numero2} = ${numero1+numero2}`
+}
