a2f3829851a0e9e77667fb79fc3964d67c016d98
MateusGCN <mateus.neves@sou.inteli.edu.br>
2022-06-02 22:17:26 -0300

    exercicio 5 da semana7

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex 5/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex 5/script.js
@@ -1,14 +1,46 @@
 function criar(){
-var nalunos = document.getElementById("alunos").value
-var lista = ""
-var nprova=[]
-var ntrabalho=[]
-for (let i = 1 ; i <= nlunos ; i++){
-lista+='<input type ="number" placeholder ="prova" id="p'+i + '"></input><input type ="number" placeholder ="trabalho" id="t'+i + '"></input> <br>'
-nprova.push(document.getElementById("p"+i).value)
-ntrabalho.push(document.getElementById("t"+i).value)
-}
-document.getElementById("1").innerHTML=lista
+    nalunos = document.getElementById("alunos").value
+    nprova=[]
+    ntrabalho=[]
+    var lista = ""
+    for (let i = 1 ; i <= nalunos ; i++){
+        lista+='<input type ="number" placeholder ="prova" id="p'+i + '"></input><input type ="number" placeholder ="trabalho" id="t'+i + '"></input> <br>'
+    }
+    document.getElementById("1").innerHTML=lista
 }
 
-fun
+function calc(){
+    mediasinviduais=[]
+    mediageral=""
+    mediaprova=""
+    mediatrabalho=""
+    for (let i = 1 ; i <= nalunos ; i++){
+        nprova.push(Number(document.getElementById("p"+i).value))
+        ntrabalho.push(Number(document.getElementById("t"+i).value))
+    }
+    for (let i = 0 ; i <= nalunos-1 ; i++){
+        mediasinviduais.push(Number((nprova[i]*2+ntrabalho[i]*3)/5))
+        mediageral=Number((mediasinviduais[i]+mediageral))
+        mediaprova=Number((nprova[i]+mediaprova))
+        mediatrabalho=Number((ntrabalho[i]+mediatrabalho))
+    }
+    mediageral=parseFloat(mediageral/nalunos)
+    mediaprova=parseFloat(mediaprova/nalunos)
+    mediatrabalho=parseFloat(mediatrabalho/nalunos)
+    maiorprova=Math.max(...nprova)
+    menorprova=Math.min(...nprova)
+    maiortrabalho=Math.max(...ntrabalho)
+    menortrabalho=Math.min(...ntrabalho)
+
+
+    console.log(nprova)
+    console.log(ntrabalho)
+    console.log(mediasinviduais)
+    console.log(mediageral)
+    console.log(mediaprova)
+    console.log(mediatrabalho)
+    console.log(maiorprova)
+    console.log(menorprova)
+    console.log(maiortrabalho)
+    console.log(menortrabalho)
+}
